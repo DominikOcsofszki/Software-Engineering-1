f0096caa831c5ed1bbf467ee2294b6cbc7042ae0
Jakob Stuehn <jakob.stuehn@smail.inf.h-brs.de>
2022-06-01 15:11:38 +0200

    add json builder tests

--- src/test/java/parkhouse/CarTest.java
+++ src/test/java/parkhouse/CarTest.java
@@ -3,7 +3,10 @@ package parkhouse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.json.JsonArray;
+import javax.json.JsonValue;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,9 +14,10 @@ class CarTest {
     CarIF c1;
     CarIF c2;
 
-    String[] paramsKunde1e = {"1","1650969214942","_","_","9fbb53684b77f16f9e88faa9e7d63d2b","#0c0f15","1","Frau","PKW","SU-S 8","16509697749492"};;
+    String[] paramsKunde1e = {"1","1650969214942","_","_","9fbb53684b77f16f9e88faa9e7d63d2b","#0c0f15","1","Frau","PKW","SU-S 8","16509697749492"};
     String[] paramsKunde6e = {"6","1650969214942","_","_","9fbb53684b77f16f9e88faa9e7d63d2b","#0c0f15","9","Frau","PKW","SU-S 8","16509697749492"};
-    String[] params6l = {"6","1650969214942","8","8","9fbb53684b77f16f9e88faa9e7d63d2b","#0c0f15","9","Frau","PKW","SU-S 8","16509697749492"};;
+    String[] params1l = {"1","1650969214942","5","5","9fbb53684b77f16f9e88faa9e7d63d2b","#0c0f15","1","Frau","PKW","SU-S 8","16509697749492"};
+    String[] params6l = {"6","1650969214942","8","8","9fbb53684b77f16f9e88faa9e7d63d2b","#0c0f15","9","Frau","PKW","SU-S 8","16509697749492"};
 
     @BeforeEach
     void setUp() {
@@ -61,5 +65,23 @@ class CarTest {
     @Test
     void testToString() {
         assertEquals(Arrays.toString(paramsKunde1e), c1.toString());
+    }
+
+    @Test
+    void asNrArrayTest() {
+        List<CarIF> cars = Arrays.asList(c1, c2);
+        JsonArray nr = Car.getNrArray(cars);
+        assertEquals("1", nr.get(0).toString());
+        assertEquals("6", nr.get(1).toString());
+    }
+
+    @Test
+    void asDurationArrayTest() {
+        c1.updateParams(params1l);
+        c2.updateParams(params6l);
+        List<CarIF> cars = Arrays.asList(c1, c2);
+        JsonArray nr = Car.getDurationArray(cars);
+        assertEquals("5", nr.get(0).toString());
+        assertEquals("8", nr.get(1).toString());
     }
 }
