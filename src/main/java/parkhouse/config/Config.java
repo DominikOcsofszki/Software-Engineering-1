beb30c243896d8501694a2aa9ff108e9fda36848
TollheitsTobi <tobias.papen@smail.inf.h-brs.de>
2022-06-06 11:26:38 +0200

    added simulation speed in config

--- src/main/java/parkhouse/config/Config.java
+++ src/main/java/parkhouse/config/Config.java
@@ -4,5 +4,6 @@ public class Config {
 
     public static final Double PRICE = 0.5;
     public static final Double FACTOR_PRICE_VIEW = 10000.;       //price factor for showing on the website _do
+    public static final Double SIMULATION_SPEED = 1000d;
 
 }
