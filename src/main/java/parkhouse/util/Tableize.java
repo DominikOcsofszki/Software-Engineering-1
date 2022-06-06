5f5ad9bb6c1bfb9b87dc648ef5d7ef246585e565
Jakob Stuehn <jakob.stuehn@smail.inf.h-brs.de>
2022-06-06 13:02:32 +0200

    add table() Test

--- src/main/java/parkhouse/util/Tableize.java
+++ src/main/java/parkhouse/util/Tableize.java
@@ -3,10 +3,11 @@ package parkhouse.util;
 public abstract class Tableize {
 
     public static String table(String[] headers, String[][] rows) {
-        StringBuilder sb = new StringBuilder("<table>");
+        StringBuilder sb = new StringBuilder("<table><tr>");
         for (String h : headers) {
             sb.append("<th>").append(h).append("</th>");
         }
+        sb.append("</tr>");
         for (String[] row : rows) {
             sb.append("<tr>");
             for (String r : row) {
@@ -14,6 +15,7 @@ public abstract class Tableize {
             }
             sb.append("</tr>");
         }
+        sb.append("</table>");
         return sb.toString();
     }
 }
