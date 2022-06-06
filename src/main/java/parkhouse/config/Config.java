16eb9b803e4e0cfdaa7653b718e0ede1ae6f7d4a
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2022-06-06 12:45:14 +0200

    changed SIMULATION_SPEED to String and 100, the lower the faster!

--- src/main/java/parkhouse/config/Config.java
+++ src/main/java/parkhouse/config/Config.java
@@ -4,6 +4,6 @@ public class Config {
 
     public static final Double PRICE = 0.5;
     public static final Double FACTOR_PRICE_VIEW = 10000.;       //price factor for showing on the website _do
-    public static final Integer SIMULATION_SPEED = 1000;
+    public static final String SIMULATION_SPEED = "100";
 
 }
