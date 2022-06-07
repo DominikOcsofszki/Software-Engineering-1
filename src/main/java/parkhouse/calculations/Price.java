6b81f42bd588e3653ec1dc174966754bea9a8dbe
Jakob Stuehn <jakob.stuehn@smail.inf.h-brs.de>
2022-06-07 21:37:57 +0200

    implement complex price factors

--- src/main/java/parkhouse/calculations/Price.java
+++ src/main/java/parkhouse/calculations/Price.java
@@ -24,19 +24,25 @@ public class Price {
 
     public static double priceFactor(ICar car) {
         List<Double> factors = new ArrayList<>();
+        List<String> applied = new ArrayList<>();
         for (String PF : Config.PRICE_FACTOR) {
-            String[] f = PF.split(":");
-            Double factor = Double.parseDouble(f[1]);
-            String[] types = f[0].split("\\.");
-            boolean match = true;
-            for (String t : types) {
-                if (!t.equals(car.type()) && !t.equals(car.category())) {
-                    match = false;
-                    break;
+            if (PF.matches("[A-Za-z]+\\.[A-Za-z]+:[0-9.]+")) {
+                String[] f = PF.split(":");
+                Double factor = Double.parseDouble(f[1]);
+                String[] types = f[0].split("\\.", 1);
+                if (types[0].equals(car.category()) && types[1].equals(car.type())) {
+                    factors.add(factor);
+                    applied.add(types[0]);
                 }
             }
-            if (match) {
-                factors.add(factor);
+        }
+        for (String PF : Config.PRICE_FACTOR) {
+            if (PF.matches("[A-Za-z]+:[0-9.]+")) {
+                String[] f = PF.split(":");
+                Double factor = Double.parseDouble(f[1]);
+                if ((f[0].equals(car.category()) || f[0].equals(car.type())) && !applied.contains(f[0])) {
+                    factors.add(factor);
+                }
             }
         }
         return factors.stream().reduce(1d, (x, y) -> x * y);
