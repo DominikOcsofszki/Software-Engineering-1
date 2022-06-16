a642263e65ef386fcd932decdfb6160667a8277e
Jakob Stuehn <jakob.stuehn@smail.inf.h-brs.de>
2022-06-16 17:41:43 +0200

    rework Finder

--- src/main/java/parkhouse/util/Finder.java
+++ src/main/java/parkhouse/util/Finder.java
@@ -5,6 +5,7 @@ import parkhouse.controller.IParkingController;
 
 import java.util.NoSuchElementException;
 import java.util.function.Function;
+import java.util.stream.StreamSupport;
 
 public class Finder {
 
@@ -12,12 +13,17 @@ public class Finder {
     }
 
     public static ICar findCar(Iterable<ICar> iter, Function<ICar, Object> loc, Object id) {
+        /*
         for (ICar c : iter) {
             if (loc.apply(c).equals(id)) {
                 return c;
             }
         }
         throw new NoSuchElementException();
+        */
+        return StreamSupport.stream(iter.spliterator(), false)
+                .filter(c -> (loc.apply(c).equals(id)))
+                .findFirst().orElseThrow();
     }
 
     public static ICar findICarForTicket(IParkingController controller, String plateSearching) {
