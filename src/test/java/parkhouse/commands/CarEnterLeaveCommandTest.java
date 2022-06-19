83f9ed80c1fd3e66599d8d54c7c0acd6b34d068a
Tobias Papen <tobias.papen@smail.inf.h-brs.de>
2022-06-19 12:37:06 +0200

    changed CarEnterCommandTest and CarLeaveCommandTest

--- src/test/java/parkhouse/commands/CarEnterLeaveCommandTest.java
+++ src/test/java/parkhouse/commands/CarEnterLeaveCommandTest.java
@@ -1,9 +1,8 @@
 package parkhouse.commands;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 import parkhouse.Data;
 import parkhouse.car.Car;
 import parkhouse.car.ICar;
@@ -13,9 +12,7 @@ import parkhouse.controller.ParkingController;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-public class CarLeaveCommandTest {
+public class CarEnterLeaveCommandTest {
 
     private final List<String[]> params = Data.paramsDuration();
     private  List<ICar> carsList = new ArrayList<>();
@@ -39,7 +36,37 @@ public class CarLeaveCommandTest {
     }
 
     @Test
-    @DisplayName("Test if the car is in the list using activate()")
+    @DisplayName("CarEnterCommand/Test if the car is in the list using activate()")
+    void carEnterCommand_executeCarIsInList_test() {
+        for(int i = 0; i < carsList.size(); i++) {
+            commander.queue(new CarEnterCommand(carsList.get(i), controller));
+            commander.activate();
+            assertEquals(i + 1, controller.getCars().size());
+        }
+    }
+
+    @Test
+    @DisplayName("CarEnterCommand/Test if the car is not in the list using undo()")
+    void carEnterCommand_undoCarIsNotInList_test() {
+        for(int i = 0; i < carsList.size(); i++) {
+            commander.queue(new CarEnterCommand(carsList.get(i), controller));
+            commander.activate();
+            assertEquals(i + 1, controller.getCars().size());
+        }
+        for(int i = 0; i < carsList.size(); i++) {
+            commander.undo();
+            assertEquals(carsList.size() - 1 - i, controller.getCars().size());
+        }
+    }
+
+    @Test
+    @DisplayName("CarEnterCommand/Test if undo throws IllegalArgumentException if the car is not in the list")
+    void carEnterCommand_undoWithoutExecute_test() {
+        assertThrows(IllegalArgumentException.class, commander::undo);
+    }
+
+    @Test
+    @DisplayName("CarLeaveCommand/Test if the car is in the list using activate()")
     void carLeaveCommand_executeCarIsInList_test() {
         for(ICar car : carsList) {
             controller.addCar(car);
@@ -53,7 +80,7 @@ public class CarLeaveCommandTest {
     }
 
     @Test
-    @DisplayName("Test if the car is not in the list using undo()")
+    @DisplayName("CarLeaveCommand/Test if the car is not in the list using undo()")
     void carLeaveCommand_undoCarIsNotInList_test() {
         for(ICar car : carsList) {
             controller.addCar(car);
@@ -70,9 +97,8 @@ public class CarLeaveCommandTest {
     }
 
     @Test
-    @DisplayName("Test if undo throws IllegalArgumentException if the car is not in the list")
+    @DisplayName("CarLeaveCommand/Test if undo throws IllegalArgumentException if the car is not in the list")
     void carLeaveCommand_undoWithoutExecute_test() {
         assertThrows(IllegalArgumentException.class, commander::undo);
     }
-
 }
