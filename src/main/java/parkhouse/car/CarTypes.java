7ebd979c527425030de9eda82eaee49e19e000e3
Jakob Stuehn <jakob.stuehn@smail.inf.h-brs.de>
2022-06-22 01:51:22 +0200

    expand multiton CarTypes

--- src/main/java/parkhouse/car/CarTypes.java
+++ src/main/java/parkhouse/car/CarTypes.java
@@ -1,31 +1,30 @@
 package parkhouse.car;
 
-import parkhouse.logging.Log;
-
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
 
 public class CarTypes {
 
-    private static final Map<String, CarTypes> instances = new HashMap<>();
+    private static final Map<String, CarTypes> types = new HashMap<>();
+    private double factor;
 
-    private CarTypes(){
+    private CarTypes() {
+    }
+
+    private CarTypes(double factor) {
+        this.factor = factor;
     }
 
     public static CarTypes getInstance(String key){
-        synchronized (instances){
-
-            CarTypes instance = instances.get(key);
-
-            if(instance == null){
-                instance = new CarTypes();
-                instances.put(key, instance);
-                Log.getLogger().log(Level.INFO, "Created new Car instance: " + key);
+            CarTypes type = types.get(key);
+            if (type == null){
+                type = new CarTypes();
+                types.put(key, type);
             }
-            return instance;
-        }
+            return type;
     }
 
-
+    public double getFactor() {
+        return factor;
+    }
 }
