39bbc9484f11b14a92681e8ca4ddaf72ffa9d7a6
Jakob Stuehn <jakob.stuehn@smail.inf.h-brs.de>
2022-07-05 15:16:29 +0200

    fix last code smells

--- src/test/java/parkhouse/car/CarTypesTest.java
+++ src/test/java/parkhouse/car/CarTypesTest.java
@@ -17,58 +17,58 @@ public class CarTypesTest {
     private static final String TRIKE = "trike";
     private static final String PICKUP = "pickup";
 
-    private CarTypes pkw;
-    private CarTypes quad;
-    private CarTypes trike;
-    private CarTypes pickup;
+    private CarTypes typePkw;
+    private CarTypes typeQuad;
+    private CarTypes typeTrike;
+    private CarTypes typePickup;
 
     @BeforeEach
     void setup(){
-        pkw = CarTypes.getInstance(PKW);
-        quad = CarTypes.getInstance(QUAD);
-        trike = CarTypes.getInstance(TRIKE);
-        pickup = CarTypes.getInstance(PICKUP);
+        typePkw = CarTypes.getInstance(PKW);
+        typeQuad = CarTypes.getInstance(QUAD);
+        typeTrike = CarTypes.getInstance(TRIKE);
+        typePickup = CarTypes.getInstance(PICKUP);
     }
 
     @Test
     @DisplayName("getInstance: returns the instance back")
     void getInstanceReturnsSameInstanceInstanceIsTheSame(){
-        Assertions.assertEquals(pkw, CarTypes.getInstance(PKW));
-        Assertions.assertEquals(quad, CarTypes.getInstance(QUAD));
-        Assertions.assertEquals(trike, CarTypes.getInstance(TRIKE));
-        Assertions.assertEquals(pickup, CarTypes.getInstance(PICKUP));
+        Assertions.assertEquals(typePkw, CarTypes.getInstance(PKW));
+        Assertions.assertEquals(typeQuad, CarTypes.getInstance(QUAD));
+        Assertions.assertEquals(typeTrike, CarTypes.getInstance(TRIKE));
+        Assertions.assertEquals(typePickup, CarTypes.getInstance(PICKUP));
 
     }
     @Test
     @DisplayName("getInstance: not the same type")
     void getInstanceReturnsSameInstanceInstanceNotTheSame() {
-        Assertions.assertNotEquals(pkw, CarTypes.getInstance(QUAD));
-        Assertions.assertNotEquals(quad, CarTypes.getInstance(PKW));
-        Assertions.assertNotEquals(trike, CarTypes.getInstance(PICKUP));
-        Assertions.assertNotEquals(pickup, CarTypes.getInstance(TRIKE));
+        Assertions.assertNotEquals(typePkw, CarTypes.getInstance(QUAD));
+        Assertions.assertNotEquals(typeQuad, CarTypes.getInstance(PKW));
+        Assertions.assertNotEquals(typeTrike, CarTypes.getInstance(PICKUP));
+        Assertions.assertNotEquals(typePickup, CarTypes.getInstance(TRIKE));
 
-        Assertions.assertNotEquals(pkw, CarTypes.getInstance(PICKUP));
-        Assertions.assertNotEquals(quad, CarTypes.getInstance(TRIKE));
-        Assertions.assertNotEquals(trike, CarTypes.getInstance(PKW));
-        Assertions.assertNotEquals(pickup, CarTypes.getInstance(QUAD));
+        Assertions.assertNotEquals(typePkw, CarTypes.getInstance(PICKUP));
+        Assertions.assertNotEquals(typeQuad, CarTypes.getInstance(TRIKE));
+        Assertions.assertNotEquals(typeTrike, CarTypes.getInstance(PKW));
+        Assertions.assertNotEquals(typePickup, CarTypes.getInstance(QUAD));
 
-        Assertions.assertNotEquals(pkw, CarTypes.getInstance(TRIKE));
-        Assertions.assertNotEquals(quad, CarTypes.getInstance(PICKUP));
-        Assertions.assertNotEquals(trike, CarTypes.getInstance(QUAD));
-        Assertions.assertNotEquals(pickup, CarTypes.getInstance(PKW));
+        Assertions.assertNotEquals(typePkw, CarTypes.getInstance(TRIKE));
+        Assertions.assertNotEquals(typeQuad, CarTypes.getInstance(PICKUP));
+        Assertions.assertNotEquals(typeTrike, CarTypes.getInstance(QUAD));
+        Assertions.assertNotEquals(typePickup, CarTypes.getInstance(PKW));
 
        }
 
     @Test
     @DisplayName("get/setFactor:sets and returns expectet value ")
     void getFactorReturnsExpectetValueIsTheSame(){
-        pkw.setFactor(2.0);
-        Assertions.assertEquals(pkw.getFactor(),2.0);
+        typePkw.setFactor(2.0);
+        Assertions.assertEquals(typePkw.getFactor(),2.0);
     }
     @Test
     @DisplayName("get/setFactor: check for illegalArgumentsException")
     void getFactorCheckIfExpectetValueIsTheSame(){
-        Assertions.assertThrows(IllegalArgumentException.class,()->pkw.setFactor(0));
-        Assertions.assertThrows(IllegalArgumentException.class,()->pkw.setFactor(-1));
+        Assertions.assertThrows(IllegalArgumentException.class,()-> typePkw.setFactor(0));
+        Assertions.assertThrows(IllegalArgumentException.class,()-> typePkw.setFactor(-1));
     }
 }
