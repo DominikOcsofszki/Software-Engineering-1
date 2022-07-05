62e54586600f45ba16f405591669bd26bc322da7
Jakob Stuehn <jakob.stuehn@smail.inf.h-brs.de>
2022-07-05 13:37:42 +0200

    rework priceFactor method

--- src/main/java/parkhouse/calculations/Price.java
+++ src/main/java/parkhouse/calculations/Price.java
@@ -3,10 +3,7 @@ package parkhouse.calculations;
 import parkhouse.car.ICar;
 import parkhouse.config.Config;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 public class Price {
 
@@ -22,31 +19,27 @@ public class Price {
 
     public static double priceFactor(ICar car) {
         List<String> priceFactors = new ArrayList<>(Arrays.asList(Config.PRICE_FACTOR));
-        for (String PF : priceFactors) {
-            if (PF.matches("^[A-Za-z]+\\.[A-Za-z]+:[0-9.]+")) {
-                String[] f = PF.split(":");
-                String[] types = f[0].split("\\.");
-                if (types[0].equals(car.category()) && types[1].equals(car.type())) {
-                    return Double.parseDouble(f[1]);
-                }
-            }
-        }
-        for (String PF : Config.PRICE_FACTOR) {
-            if (PF.matches("^[A-Za-z]+:[0-9.]+")) {
-                String[] f = PF.split(":");
-                if ((f[0].equals(car.category()))) {
-                    return Double.parseDouble(f[1]);
-                }
-            }
-        }
-        for (String PF : Config.PRICE_FACTOR) {
-            if (PF.matches("^[A-Za-z]+:[0-9.]+")) {
-                String[] f = PF.split(":");
-                if ((f[0].equals(car.type()))) {
-                    return Double.parseDouble(f[1]);
-                }
-            }
-        }
+
+        OptionalDouble categoryType = priceFactors.stream()
+                .filter(p -> p.matches("^[A-Za-z]+\\.[A-Za-z]+:[0-9.]+"))
+                .map(p -> p.split(":"))
+                .filter(p -> p[0].split("\\.")[0].equals(car.category()))
+                .filter(p -> p[0].split("\\.")[1].equals(car.type()))
+                .mapToDouble(p -> Double.parseDouble(p[1])).findFirst();
+        if (categoryType.isPresent()) return categoryType.getAsDouble();
+
+        OptionalDouble category = priceFactors.stream()
+                .filter(p -> p.matches("^[A-Za-z]+:[0-9.]+"))
+                .map(p -> p.split(":")).filter(p -> p[0].equals(car.category()))
+                .mapToDouble(p -> Double.parseDouble(p[1])).findFirst();
+        if (category.isPresent()) return category.getAsDouble();
+
+        OptionalDouble type = priceFactors.stream()
+                .filter(p -> p.matches("^[A-Za-z]+:[0-9.]+"))
+                .map(p -> p.split(":")).filter(p -> p[0].equals(car.type()))
+                .mapToDouble(p -> Double.parseDouble(p[1])).findFirst();
+        if (type.isPresent()) return type.getAsDouble();
+
         return 1d;
     }
 
